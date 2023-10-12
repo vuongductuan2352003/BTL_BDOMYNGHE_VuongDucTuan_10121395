54fd2cf6e5a96d65fef26b6639af8ef280f8acfa
Vương Đức Tuấn <146206322+vuongductuan2352003@users.noreply.github.com>
2023-10-12 10:12:46 +0700

    Add files via upload

--- DATABASE_DoGoMyNghe.sql
+++ DATABASE_DoGoMyNghe.sql
@@ -3,85 +3,379 @@ go
 use DoGoMyNghe
 
 --Tạo bảng khách hàng
-
-CREATE TABLE KHACHHANG(
-	MAKH CHAR(10) PRIMARY KEY, --MAKH SINH TU DONG
-    TENKH NVARCHAR(50) NOT NULL,
-    SDT CHAR(10) NOT NULL,
-	DIACHI NVARCHAR(100) NOT NULL,
-	EMAIL NVARCHAR(100) NOT NULL,
-)
-
---TẠO BẢNG NHÂN VIÊN
-
-CREATE TABLE NHANVIEN
+go
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[KhachHang](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[TenKH] [nvarchar](50) NULL,
+	[GioiTinh] [nvarchar](30) NOT NULL,
+	[DiaChi] [nvarchar](250) NULL,
+	[SDT] [nvarchar](50) NULL,
+	[Email] [nvarchar](250) NULL,
+PRIMARY KEY CLUSTERED 
 (
-	MANV CHAR(10) PRIMARY KEY,
-    MATKHAU CHAR(10) NOT NULL,
-    TENNV NVARCHAR(50) NOT NULL,
-	SDT CHAR(10) NOT NULL,
-    NGAYSINH DATETIME NOT NULL,
-	DIACHI TEXT,
-	EMAIL VARCHAR(100) NOT NULL
-)
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
---TẠO BẢNG DANH MỤC
-CREATE TABLE DANHMUC
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DanhMuc](
+	[MaDanhMuc] [int] IDENTITY(1,1) NOT NULL,
+	[TenDanhMuc] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
 (
-	MADM CHAR(10) PRIMARY KEY,
-	TENDM NVARCHAR(50) NOT NULL,
-)
+	[MaDanhMuc] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
 
---TẠO BẢNG SẢN PHẨM
- 
- CREATE TABLE SANPHAM
- (
-	MADM CHAR(10) NOT NULL FOREIGN KEY REFERENCES	 DANHMUC(MADM)
-   ON DELETE CASCADE ON UPDATE CASCADE,
-	 MASP CHAR(10) PRIMARY KEY,
-    TENSP NVARCHAR(50)NOT NULL,
-	MOTA TEXT,
-	GIABAN FLOAT,
-	SOLUONG INT,
- )
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[LoaiTaiKhoan](
+	[MaLoai] [int] IDENTITY(1,1) NOT NULL,
+	[TenLoai] [nvarchar](50) NULL,
+	[MoTa] [nvarchar](250) NULL,
+ CONSTRAINT [PK_TypeAccounts] PRIMARY KEY CLUSTERED 
+(
+	[MaLoai] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
- --TẠO BẢNG HÓA ĐƠN BÁN
-CREATE TABLE HOADONBAN
- (
-	 MAHDB CHAR(10) PRIMARY KEY,
-    NGAYBAN DATETIME NOT NULL,
-	 MADM CHAR(10) NOT NULL FOREIGN KEY REFERENCES	 DANHMUC(MADM),
-    MASP CHAR(10) NOT NULL FOREIGN KEY REFERENCES SANPHAM(MASP)
-   ON DELETE CASCADE ON UPDATE CASCADE,
-     MANV CHAR(10) NOT NULL FOREIGN KEY REFERENCES NHANVIEN(MANV)
-   ON DELETE CASCADE ON UPDATE CASCADE,
-    MAKH CHAR(10) NOT NULL FOREIGN KEY REFERENCES KHACHHANG(MAKH)
-   ON DELETE CASCADE ON UPDATE CASCADE,
- )
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TaiKhoan](
+	[MaTaiKhoan] [int] IDENTITY(1,1) NOT NULL,
+	[LoaiTaiKhoan] [int] NULL,
+	[TenTaiKhoan] [nvarchar](50) NULL,
+	[MatKhau] [nvarchar](50) NULL,
+	[Email] [nvarchar](150) NULL,
+ CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED 
+(
+	[MaTaiKhoan] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
 
- --CHI TIẾT HÓA ĐƠN BÁN
- CREATE TABLE CHITIETHDB
- (
-	MAHDB CHAR(10) NOT NULL FOREIGN KEY REFERENCES HOADONBAN(MAHDB)
-   ON DELETE CASCADE ON UPDATE CASCADE,
-   TENNV NVARCHAR(50) NOT NULL,
-   TENKH NVARCHAR(50) NOT NULL,
-   TENDM NVARCHAR(50) NOT NULL,
-   TENSP NVARCHAR(50) NOT NULL,
-    SOLUONG INT,
-	GIABAN FLOAT,
-	TONGTIEN FLOAT
- )
- select * from KHACHHANG
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ChiTietTaiKhoan](
+	[MaChitietTaiKhoan] [int] IDENTITY(1,1) NOT NULL,
+	[MaTaiKhoan] [int] NULL,
+	[HoTen] [nvarchar](50) NULL,
+	[DiaChi] [nvarchar](250) NULL,
+	[SoDienThoai] [nvarchar](11) NULL,
+	[AnhDaiDien] [nvarchar](500) NULL,
+ CONSTRAINT [PK_InformationAccounts] PRIMARY KEY CLUSTERED 
+(
+	[MaChitietTaiKhoan] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 
- INSERT INTO KHACHHANG VALUES('KH1',N'VƯƠNG ĐỨC TUẤN','0135451251',N'CẨM GIÀNG-HẢII DƯƠNG','vuongductuan@gmail.com')
-INSERT INTO KHACHHANG VALUES('KH2',N'NGUYỄN VĂN TRƯỜNG','0153541252',N'KHOÁI CHÂU-HƯNG YÊN')
-INSERT INTO KHACHHANG VALUES('KH3',N'ĐINH QUỐC TRUNG','0135541254',N'THÀNH PHỐ HƯNG YÊN')
-INSERT INTO KHACHHANG VALUES('KH4',N'PHẠM TIẾN THÀNH','0135454251',N'ÂN THI-HƯNG YÊN')
-INSERT INTO KHACHHANG VALUES('KH5',N'HOÀNG NGUYỆT NHI','0135541251',N'ÂN THI-HƯNG YÊN')
-INSERT INTO KHACHHANG VALUES('KH6',N'VŨ MINH HIẾU','0130012551',N'PHÚ THỌ')
-INSERT INTO KHACHHANG VALUES('KH7',N'DƯƠNG ĐỨC NGUYÊN','0135541111',N'THÀNH PHỐ HƯNG YÊN')
-INSERT INTO KHACHHANG VALUES('KH8',N'VƯƠNG ĐỨC DŨNG','0135415251',N'CẨM GIÀNG-HẢI DƯƠNG')
-INSERT INTO KHACHHANG VALUES('KH9',N'PHẠM THỊ ÁNH NGỌC','0135441251',N'NINH BÌNH')
-INSERT INTO KHACHHANG VALUES('KH10',N'NGUYỂN THÚY HIỀN','0134541231',N'KHOÁI CHÂU-HƯNG YÊN')
+
+
+
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SanPham](
+	[MaSanPham] [int] IDENTITY(1,1) NOT NULL,
+	[MaDanhMuc] [int] NULL,
+	[TenSanPham] [nvarchar](150) NULL,
+	[AnhDaiDien] [nvarchar](350) NULL,
+	[Gia] [decimal](18, 0) NULL,
+	[GiaGiam] [decimal](18, 0) NULL,
+	[SoLuong] [int] NULL,
+	[TrangThai] [bit] NULL,
+	
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[MaSanPham] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ChiTietSanPham](
+	[MaChiTietSanPham] [int] IDENTITY(1,1) NOT NULL,
+	[MaSanPham] [int] NULL,
+	[MaNgheNhanGiaCong] [int] NULL,
+	[MoTa] [nvarchar](350) NOT NULL,
+	[ChiTiet] [nvarchar](max) NULL,
+ CONSTRAINT [PK_DetailProducts] PRIMARY KEY CLUSTERED 
+(
+	[MaChiTietSanPham] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NgheNhanGiaCong](
+	[MaNgheNhanGiaCong] [int] IDENTITY(1,1) NOT NULL,
+	[TenNgheNhanGiaCong] [nvarchar](50) NULL,
+	[LinkWeb] [nvarchar](max) NULL,
+	[AnhDaiDien] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[MaNgheNhanGiaCong] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NhaCungCap](
+	[MaNhaCungCap] [int] IDENTITY(1,1) NOT NULL,
+	[TenNhaCungCap] [nvarchar](250) NULL,
+	[DiaChi] [nvarchar](max) NULL,
+	[SoDienThoai] [nvarchar](50) NULL,
+	[Fax] [nvarchar](50) NULL,
+	[MoTa] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[MaNhaCungCap] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SanPham_NhaCungCap](
+	[MaSanPham] [int] NOT NULL,
+	[MaNhaCungCap] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[MaNhaCungCap] ASC,
+	[MaSanPham] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[HoaDon](
+	[MaHoaDon] [int] IDENTITY(1,1) NOT NULL,
+	[TrangThai] [bit] NULL,
+	[NgayTao] [datetime] NULL,
+	[NgayDuyet] [datetime] NULL,
+	[TongGia] [decimal](18, 0) NULL,
+	[TenKH] [nvarchar](50) NULL,
+	[GioiTinh] [bit] NOT NULL,
+	[Diachi] [nvarchar](250) NULL,
+	[Email] [nvarchar](50) NULL,
+	[SDT] [nvarchar](50) NULL,
+	[DiaChiGiaoHang] [nvarchar](350) NULL,
+	[ThoiGianGiaoHang] [datetime] NULL,
+ CONSTRAINT [PK_Bills] PRIMARY KEY CLUSTERED 
+(
+	[MaHoaDon] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ChiTietHoaDon](
+	[MaChiTietHoaDon] [int] IDENTITY(1,1) NOT NULL,
+	[MaHoaDon] [int] NULL,
+	[MaSanPham] [int] NULL,
+	[SoLuong] [int] NULL,
+	[TongGia] [decimal](18, 0) NULL,
+ CONSTRAINT [PK_DetailBill] PRIMARY KEY CLUSTERED 
+(
+	[MaChiTietHoaDon] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[HoaDonNhap](
+	[MaHoaDon] [int] IDENTITY(1,1) NOT NULL,
+	[MaNhaCungCap] [int] NULL,
+	[NgayTao] [datetime] NULL,
+	[KieuThanhToan] [nvarchar](max) NULL,
+	[MaTaiKhoan] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[MaHoaDon] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ChiTietHoaDonNhap](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MaHoaDon] [int] NULL,
+	[MaSanPham] [int] NULL,
+	[SoLuong] [int] NULL,
+	[DonViTinh] [nvarchar](50) NULL,
+	[GiaNhap] [decimal](18, 0) NULL,
+	[TongTien] [decimal](18, 0) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+--bảng chi tiết hóa đơn liên kết bảng hóa đơn qua mã hóa đơn 
+GO
+ALTER TABLE [dbo].[ChiTietHoaDon]  WITH CHECK ADD  CONSTRAINT [FK_DetailBill_Bills] FOREIGN KEY([MaHoaDon])
+REFERENCES [dbo].[HoaDon] ([MaHoaDon])
+GO
+ALTER TABLE [dbo].[ChiTietHoaDon] CHECK CONSTRAINT [FK_DetailBill_Bills]
+
+--bảng chi tiết hóa đơn liên kết bảng sản phẩm qua mã sản phẩm
+
+GO
+ALTER TABLE [dbo].[ChiTietHoaDon]  WITH CHECK ADD  CONSTRAINT [FK_DetailBill_Products] FOREIGN KEY([MaSanPham])
+REFERENCES [dbo].[SanPham] ([MaSanPham])
+GO
+ALTER TABLE [dbo].[ChiTietHoaDon] CHECK CONSTRAINT [FK_DetailBill_Products]
+--bảng nghệ nhân gia công liên kết bảng chi tiết sản phẩm qua mã nghệ nhân gia công
+GO
+ALTER TABLE [dbo].[ChiTietSanPham]  WITH CHECK ADD  CONSTRAINT [FK_ChiTietSanPham_NgheNhanGiaCong] FOREIGN KEY([MaNgheNhanGiaCong])
+REFERENCES [dbo].[NgheNhanGiaCong] ([MaNgheNhanGiaCong])
+GO
+ALTER TABLE [dbo].[ChiTietSanPham] CHECK CONSTRAINT [FK_ChiTietSanPham_NgheNhanGiaCong]
+--bảng chi tiết sản phẩm liên kết bảng sản phẩm qua mã sản phẩm 
+GO
+ALTER TABLE [dbo].[ChiTietSanPham]  WITH CHECK ADD  CONSTRAINT [FK_DetailProducts_Products] FOREIGN KEY([MaSanPham])
+REFERENCES [dbo].[SanPham] ([MaSanPham])
+GO
+ALTER TABLE [dbo].[ChiTietSanPham] CHECK CONSTRAINT [FK_DetailProducts_Products]
+--bảng danh mục liên kết bảng sản phẩm qua mã danh mục
+GO--lỗi
+ALTER TABLE [dbo].[SanPham]  WITH CHECK ADD  CONSTRAINT [FK_Products_Categories] FOREIGN KEY([MaDanhMuc])
+REFERENCES [dbo].[DanhMuc] ([MaDanhMuc])
+GO
+ALTER TABLE [dbo].[SanPham] CHECK CONSTRAINT [FK_Products_Categories]
+
+--bảng tài khoản liên kết bảng loại tài khoản qua mã loại tài khoảnư\
+go
+ALTER TABLE [dbo].[TaiKhoan]  WITH CHECK ADD  CONSTRAINT [FK_Accounts_TypeAccounts] FOREIGN KEY([LoaiTaiKhoan])
+REFERENCES [dbo].[LoaiTaiKhoan] ([MaLoai])
+GO
+ALTER TABLE [dbo].[TaiKhoan] CHECK CONSTRAINT [FK_Accounts_TypeAccounts]
+--bảng tài khoản liên kế bảng chi tiết tài khoản qua mã tài khoản
+go
+ALTER TABLE [dbo].[ChiTietTaiKhoan]  WITH CHECK ADD  CONSTRAINT [FK_InformationAccounts_Accounts] FOREIGN KEY([MaTaiKhoan])
+REFERENCES [dbo].[TaiKhoan] ([MaTaiKhoan])
+GO
+ALTER TABLE [dbo].[ChiTietTaiKhoan] CHECK CONSTRAINT [FK_InformationAccounts_Accounts]
+--bảng tài khoản liên kết bảng hóa đơn nhập qua mã tài khoản
+GO
+ALTER TABLE [dbo].[HoaDonNhap]  WITH CHECK ADD  CONSTRAINT [FK_HoaDonNhap_TaiKhoan] FOREIGN KEY([MaTaiKhoan])
+REFERENCES [dbo].[TaiKhoan] ([MaTaiKhoan])
+GO
+ALTER TABLE [dbo].[HoaDonNhap] CHECK CONSTRAINT [FK_HoaDonNhap_TaiKhoan]
+--bảng chi tiết hóa đơn nhập liên kết bảng hóa đơn nhập qua mã hóa đơn nhập
+GO
+ALTER TABLE [dbo].[ChiTietHoaDonNhap]  WITH CHECK ADD  CONSTRAINT [FK_ChiTietHoaDonNhap_HoaDonNhap] FOREIGN KEY([MaHoaDon])
+REFERENCES [dbo].[HoaDonNhap] ([MaHoaDon])
+GO
+ALTER TABLE [dbo].[ChiTietHoaDonNhap] CHECK CONSTRAINT [FK_ChiTietHoaDonNhap_HoaDonNhap]
+--bảng chi tiết hóa đơn nhập liên kết bảng sản phẩm qua mã sản phẩm
+ALTER TABLE [dbo].[ChiTietHoaDonNhap]  WITH CHECK ADD  CONSTRAINT [FK_ChiTietHoaDonNhap_SanPham] FOREIGN KEY([MaSanPham])
+REFERENCES [dbo].[SanPham] ([MaSanPham])
+GO
+ALTER TABLE [dbo].[ChiTietHoaDonNhap] CHECK CONSTRAINT [FK_ChiTietHoaDonNhap_SanPham]
+--bảng nhà cung cấp liên kết bảng hóa đơn nhập qua mã nhà cung cấp
+GO
+ALTER TABLE [dbo].[HoaDonNhap]  WITH CHECK ADD  CONSTRAINT [FK_HoaDonNhap_NhaCungCap] FOREIGN KEY([MaNhaCungCap])
+REFERENCES [dbo].[NhaCungCap] ([MaNhaCungCap])
+GO
+ALTER TABLE [dbo].[HoaDonNhap] CHECK CONSTRAINT [FK_HoaDonNhap_NhaCungCap]
+--bảng nhà cung cấp liên kết bảng nhà cung cấp sản phẩm qua mã nhà cung cấp 
+ALTER TABLE [dbo].[SanPham_NhaCungCap]  WITH CHECK ADD  CONSTRAINT [FK_SanPham_NhaCungCap] FOREIGN KEY([MaNhaCungCap])
+REFERENCES [dbo].[NhaCungCap] ([MaNhaCungCap])
+GO
+ALTER TABLE [dbo].[SanPham_NhaCungCap] CHECK CONSTRAINT [FK_SanPham_NhaCungCap]
+--bảng sản phẩm nhà cung cấp liên kết bảng sản phẩm qua mã sản phẩm
+GO
+ALTER TABLE [dbo].[SanPham_NhaCungCap]  WITH CHECK ADD  CONSTRAINT [FK_SanPhams_NhaCungCap] FOREIGN KEY([MaSanPham])
+REFERENCES [dbo].[SanPham] ([MaSanPham])
+GO
+ALTER TABLE [dbo].[SanPham_NhaCungCap] CHECK CONSTRAINT [FK_SanPham_NhaCungCap]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- select * from KHACHHANG
+
+-- INSERT INTO KHACHHANG VALUES('KH1',N'VƯƠNG ĐỨC TUẤN','0135451251',N'CẨM GIÀNG-HẢII DƯƠNG','vuongductuan@gmail.com')
+--INSERT INTO KHACHHANG VALUES('KH2',N'NGUYỄN VĂN TRƯỜNG','0153541252',N'KHOÁI CHÂU-HƯNG YÊN')
+--INSERT INTO KHACHHANG VALUES('KH3',N'ĐINH QUỐC TRUNG','0135541254',N'THÀNH PHỐ HƯNG YÊN')
+--INSERT INTO KHACHHANG VALUES('KH4',N'PHẠM TIẾN THÀNH','0135454251',N'ÂN THI-HƯNG YÊN')
+--INSERT INTO KHACHHANG VALUES('KH5',N'HOÀNG NGUYỆT NHI','0135541251',N'ÂN THI-HƯNG YÊN')
+--INSERT INTO KHACHHANG VALUES('KH6',N'VŨ MINH HIẾU','0130012551',N'PHÚ THỌ')
+--INSERT INTO KHACHHANG VALUES('KH7',N'DƯƠNG ĐỨC NGUYÊN','0135541111',N'THÀNH PHỐ HƯNG YÊN')
+--INSERT INTO KHACHHANG VALUES('KH8',N'VƯƠNG ĐỨC DŨNG','0135415251',N'CẨM GIÀNG-HẢI DƯƠNG')
+--INSERT INTO KHACHHANG VALUES('KH9',N'PHẠM THỊ ÁNH NGỌC','0135441251',N'NINH BÌNH')
+--INSERT INTO KHACHHANG VALUES('KH10',N'NGUYỂN THÚY HIỀN','0134541231',N'KHOÁI CHÂU-HƯNG YÊN')
